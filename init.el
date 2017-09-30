f9d177f917d0be893734c081cde3fe03dfb56197
Muhammad Kaisar Arkhan <yukiisbored@disroot.org>
2017-09-30 16:54:38 +0700

    Initial commit

---
+++ init.el
@@ -0,0 +1,16 @@
+(package-initialize)
+
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+
+(when (< emacs-major-version 24)
+  (add-to-list 'package-archives
+	       '("gnu" . "http://elpa.gnu.org/packages/")))
+
+                                        ;
+;; GET OF MY LAWN REEEEEEEE
+(defconst my-custom-file "~/.emacs.d/custom.el")
+(setq custom-file my-custom-file)
+(load custom-file 'noerr)
+
+(org-babel-load-file "~/.emacs.d/config.org")
